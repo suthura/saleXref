96b18a45edc57ac010f66b0c07b18ddb786ee0db
Suthura Sudharaka <suthura@hotmail.com>
2020-03-22 13:54:41 +0530

    dashboard recent sale

--- lib/ui/MainScreens/Dashboard/dashboarspage.dart
+++ lib/ui/MainScreens/Dashboard/dashboarspage.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:salex/ui/MainScreens/Dashboard/dashboard.dart';
 import 'package:salex/ui/MainScreens/Common/fabOptions.dart';
+import 'package:salex/Controllers/ApiServices/GetMySalesService.dart';
+import 'package:salex/Models/saleModel.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -11,11 +13,29 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+List<SaleItem> saleItem = List();
+List<SaleItem> filteredSaleItem = List();
+
 final backgroundColor = Colors.transparent;
 double screenWidth, screenHeight;
 final Duration duration = const Duration(milliseconds: 200);
 
 class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    callAPI();
+  }
+
+  callAPI() {
+    GetMySalesService.getSales().then((saleFromServer) {
+      setState(() {
+        saleItem = saleFromServer;
+        filteredSaleItem = saleItem;
+        print("Sale list updated");
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,12 +46,11 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
         backgroundColor: Colors.transparent,
         body: FabCircularMenu(
-          fabOpenIcon: Icon(
-            Icons.menu,
-            size: 40,
-          ),
-          child: dashboard(context),
-          options: getOptions(context)
-        ));
+            fabOpenIcon: Icon(
+              Icons.menu,
+              size: 40,
+            ),
+            child: dashboard(context,filteredSaleItem),
+            options: getOptions(context)));
   }
 }
